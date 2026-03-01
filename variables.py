540592eed868b959f548a9a7d56ab5daba3093b0
afan099 <afansharif8150@gmail.com>
2026-03-01 20:15:41 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,12 @@
+int=10
+print(int)
+float=10.5
+print(float)
+string="Hello World"
+print(string)
+boolean=True
+print(boolean)
+if (10==int):
+    print("10 is int")
+else:
+    print("10 is not equal to int")
